6df0ceeb38df8603e8c4661919fe610fda1052c3
Skyler Clark <sclark@freolic.com>
2021-05-29 09:21:41 -0400

    Fix link issues with missing template definitions

--- Source/BetterConstructionScripts/Public/BetterConstructionScriptsInterface.h
+++ Source/BetterConstructionScripts/Public/BetterConstructionScriptsInterface.h
@@ -65,10 +65,67 @@ public:
 	bool EditorOnlyConstructionScript();	
 
 	template <class TActor>
-	static void OnConstruction(TActor *Actor);
+	static void OnConstruction(TActor *Actor)
+	{
+		if (!Actor->GetWorld())
+			return;
+
+		const EWorldType::Type WorldType = Actor->GetWorld()->WorldType;
+
+		const bool bConstructionAllowed = (WorldType != EWorldType::EditorPreview && WorldType != EWorldType::GamePreview);
+		if (!bConstructionAllowed)
+			return;
+
+		const bool isEditorWorld = (WorldType == EWorldType::Editor);
+		const bool isCached = Actor->bCachedConstructionHasRun;
+
+		OnConstruction_ConstructComponents(Actor, EBetterComponentCSTiming::RunBeforeActor);
+
+#if WITH_EDITOR
+		if (isEditorWorld)
+			Actor->Execute_EditorOnlyConstructionScript(Actor);
+#endif // #if WITH_EDITOR
+
+		if (isEditorWorld || !isCached)
+			Actor->Execute_CachedConstructionScript(Actor);
+
+		OnConstruction_ConstructComponents(Actor, EBetterComponentCSTiming::RunAfterActor);
+
+		// By saving this value into a UPROPERTY, we can distinguish between placed 
+		// and freshly spawned actors.
+		Actor->bCachedConstructionHasRun = true;
+	}
 
 private:
 	template <class TActor>
-	static void OnConstruction_ConstructComponents(TActor *Actor, EBetterComponentCSTiming Timing);
+	static void OnConstruction_ConstructComponents(TActor *Actor, EBetterComponentCSTiming Timing)
+	{
+		const bool bAllowConstruction = Actor->ComponentConstructionScript == Timing;
+		const bool bAllowEditorConstruction = Actor->ComponentEditorOnlyConstructionScript == Timing;
+		const bool bAllowCachedConstruction = Actor->ComponentCachedConstructionScript == Timing;
+
+		if (bAllowConstruction || bAllowEditorConstruction || bAllowCachedConstruction)
+		{
+			const bool bIsEditorWorld = (Actor->GetWorld()->WorldType == EWorldType::Editor);
+			const bool bIsCached = Actor->bCachedConstructionHasRun;
+
+			for (UActorComponent* Component : Actor->GetComponents())
+			{
+				if (Component->Implements<UBetterComponentConstructionScripts>())
+				{
+					if (bAllowConstruction)
+						IBetterComponentConstructionScripts::Execute_ConstructionScript(Component);
+
+#if WITH_EDITOR
+					if (bAllowEditorConstruction && bIsEditorWorld)
+						IBetterComponentConstructionScripts::Execute_EditorOnlyConstructionScript(Component);
+#endif // #if WITH_EDITOR
+
+					if (bAllowCachedConstruction && (bIsEditorWorld || !bIsCached))
+						IBetterComponentConstructionScripts::Execute_CachedConstructionScript(Component);
+				}
+			}
+		}
+	}
 
 };
